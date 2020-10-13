ecd9a6876b9ee7e2b5c91be0e38964b412108592
nicoleelarue <70534341+nicoleelarue@users.noreply.github.com>
2020-10-13 12:56:18 -0400

    working on sql and figuring out api

---
+++ app/public/js/members.app.js
@@ -0,0 +1,55 @@
+var members = new Vue({
+el: '#MembersApp',
+data: {
+  members: [],
+  member: {}
+
+},
+
+methods: {
+    fetchMembers() {
+      fetch('api/firefighters/index.php')
+      .then(response => response.json())
+      .then(json => { MembersApp.members = json })
+    },
+    handleSubmit() {
+      fetch('api/firefighters/post.php', {
+        method: 'POST',
+        body: JSON.stringify(this.member),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      window.alert("Member was created");
+      window.location.href = 'members.html';
+    },
+    handleClick(m) {
+      fetch('api/membersWithCertification/index.php', {
+        method: 'POST',
+        body: JSON.stringify(m),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      }).then(response => response.json())
+        .then(json => { memberDetailApp.memberCert = json })
+      memberDetailApp.member = m;
+    },
+    handleEdit(m) {
+      window.location.href = 'editmember.html?memberGuid='+m.memberGuid;
+    },
+    handleDelete(m) {
+      fetch('api/firefighters/delete.php', {
+        method: 'POST',
+        body: JSON.stringify(m),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      window.alert("Member was deleted");
+      window.location.href = 'members.html';
+    }
+  },
+  created() {
+    this.fetchMembers();
+  }
+});
