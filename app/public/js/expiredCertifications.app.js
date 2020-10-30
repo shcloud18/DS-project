757fd1645308b27c2d46dd281489c2a4c2eef7f2
nicoleelarue <70534341+nicoleelarue@users.noreply.github.com>
2020-10-30 12:33:17 -0400

    updates to expiredcerts

--- app/public/js/expiredCertifications.app.js
+++ app/public/js/expiredCertifications.app.js
@@ -1,4 +1,33 @@
-var expCertApp = new Vue({
-      el: '#expCertApp',
+var expiredCertificationsApp = new Vue({
+      el: '#expiredCertificationsApp',
       data: {
-        ExpiredCerts: []
+        ExpiredCertifications: [{
+          firstName: '',
+          lastName: '',
+          position: '',
+          certificationName: '',
+          renewedDate: '',
+          expirationDate: '',
+          status: ''
+        }],
+        certs: {
+          certificationName: " "
+        }
+
+    },
+    methods: {
+      fetchExpiredCertifications() {
+        fetch('api/expiredCertifications/index.php')
+        .then(response => response.json())
+        .then(json => {
+          this.ExpiredCertifications=json;
+          console.log(this.ExpiredCertifications);
+
+      });
+    },
+    created() {
+      this.fetchExpiredCertifications();
+
+    }
+  }
+});
